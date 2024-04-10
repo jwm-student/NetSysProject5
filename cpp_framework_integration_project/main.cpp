7be1fa64e382e4198285719f19b2a82757effa73
fl4m3n <107616589+fl4m3n@users.noreply.github.com>
2024-04-10 15:06:50 +0200

    set frequency and token

--- cpp_framework_integration_project/main.cpp
+++ cpp_framework_integration_project/main.cpp
@@ -18,9 +18,9 @@ std::string SERVER_ADDR = "netsys.ewi.utwente.nl"; //"127.0.0.1"
 // The port to connect to. 8954 for the simulation server
 int SERVER_PORT = 8954;
 // The frequency to connect on.
-int FREQUENCY = //TODO: Set this to your group frequency!
+int FREQUENCY = 8000;//TODO: Set this to your group frequency!
 // The token you received for your frequency range
-std::string TOKEN = "your token";
+std::string TOKEN = "cpp-05-AYKI3U9SX758O0EPJT";
 
 using namespace std;
 
