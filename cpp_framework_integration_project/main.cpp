2eec6e0ca4b86ec736282d6b9f4df015d5462136
Boaz Bouwman <b.bouwman-1@student.utwente.nl>
2024-04-12 15:27:41 +0200

    Removed last comment

--- cpp_framework_integration_project/main.cpp
+++ cpp_framework_integration_project/main.cpp
@@ -88,7 +88,6 @@ vector<vector<int>> initializeDVR(BlockingQueue< Message >*senderQueue, Blocking
 				updatedTable = true;
 			}
 			break;
-		//Cool and epic
 			case DATA:			// This means that it is a topology message
 			// Add table information to own table
 
